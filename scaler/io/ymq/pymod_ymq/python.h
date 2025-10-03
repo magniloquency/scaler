af5b9451710398b3dce1556c7e7d965cf05e7904
gxu <georgexu420@163.com>
2025-10-04 02:38:58 +0800

    Update SyncObjectStorageConnector to use ymq
    Signed-off-by: gxu <georgexu420@163.com>

--- scaler/io/ymq/pymod_ymq/python.h
+++ scaler/io/ymq/pymod_ymq/python.h
@@ -77,7 +77,7 @@ public:
     // steals a reference
     OwnedPyObject(T* ptr): _ptr(ptr) {}
 
-    OwnedPyObject(const OwnedPyObject& other) { this->_ptr = Py_XNewRef(other._ptr); }
+    OwnedPyObject(const OwnedPyObject& other) { this->_ptr = (T*)Py_XNewRef((PyObject*)other._ptr); }
     OwnedPyObject(OwnedPyObject&& other) noexcept: _ptr(other._ptr) { other._ptr = nullptr; }
     OwnedPyObject& operator=(const OwnedPyObject& other)
     {
@@ -85,7 +85,7 @@ public:
             return *this;
 
         this->free();
-        this->_ptr = Py_XNewRef(other._ptr);
+        this->_ptr = (T*)Py_XNewRef((PyObject*)other._ptr);
         return *this;
     }
     OwnedPyObject& operator=(OwnedPyObject&& other) noexcept
