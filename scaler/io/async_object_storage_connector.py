5d7a74740947e792e87879786ba09b37ea7e3752
magniloquency <197707854+magniloquency@users.noreply.github.com>
2025-10-15 23:01:33 -0400

    Apply suggestion from @magniloquency

--- scaler/io/async_object_storage_connector.py
+++ scaler/io/async_object_storage_connector.py
@@ -65,7 +65,7 @@ class PyAsyncObjectStorageConnector(AsyncObjectStorageConnector):
     def is_connected(self) -> bool:
         return self._connected_event.is_set()
 
-    def destroy(self):
+    async def destroy(self):
         if not self.is_connected():
             return
 
