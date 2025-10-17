e280f558258dfbb0a5998c923cd5260958ff2d17
magniloquency <197707854+magniloquency@users.noreply.github.com>
2025-10-17 11:52:18 -0400

    rename variables

--- tests/cpp/ymq/py_mitm/randomly_drop_packets.py
+++ tests/cpp/ymq/py_mitm/randomly_drop_packets.py
@@ -9,19 +9,19 @@ from tests.cpp.ymq.py_mitm.types import IP, AbstractMITM, TCPConnection, TunTapI
 
 
 class MITM(AbstractMITM):
-    def __init__(self, drop_pcent: str):
-        self.drop_pcent = float(drop_pcent)
-        self.consecutive_drop_limit = 3
-        self.client_consecutive_drops = 0
-        self.server_consecutive_drops = 0
+    def __init__(self, drop_percentage: str):
+        self._drop_percentage = float(drop_percentage)
+        self._consecutive_drop_limit = 3
+        self._client_consecutive_drops = 0
+        self._server_consecutive_drops = 0
 
     @property
     def can_drop_client(self) -> bool:
-        return self.client_consecutive_drops < self.consecutive_drop_limit
+        return self._client_consecutive_drops < self._consecutive_drop_limit
 
     @property
     def can_drop_server(self) -> bool:
-        return self.server_consecutive_drops < self.consecutive_drop_limit
+        return self._server_consecutive_drops < self._consecutive_drop_limit
 
     def proxy(
         self,
@@ -31,20 +31,20 @@ class MITM(AbstractMITM):
         client_conn: Optional[TCPConnection],
         server_conn: TCPConnection,
     ) -> bool:
-        drop = random.random() < self.drop_pcent
+        drop = random.random() < self._drop_percentage
 
         if sender == client_conn or client_conn is None:
             if self.can_drop_client and drop:
-                self.client_consecutive_drops += 1
+                self._client_consecutive_drops += 1
                 return False
 
-            self.client_consecutive_drops = 0
+            self._client_consecutive_drops = 0
             tuntap.send(server_conn.rewrite(pkt))
         elif sender == server_conn:
             if self.can_drop_server and drop:
-                self.server_consecutive_drops += 1
+                self._server_consecutive_drops += 1
                 return False
 
-            self.server_consecutive_drops == 0
+            self._server_consecutive_drops == 0
             tuntap.send(client_conn.rewrite(pkt))
         return True
