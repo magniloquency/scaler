e5e0a73205f2ad2b822e8736567931e0a10c8edc
magniloquency <197707854+magniloquency@users.noreply.github.com>
2025-11-13 11:01:18 -0500

    remove pipe_common.cpp

--- tests/cpp/ymq/pipe/pipe_windows.cpp
+++ tests/cpp/ymq/pipe/pipe_windows.cpp
@@ -3,6 +3,26 @@
 #include "tests/cpp/ymq/common/utils.h"
 #include "tests/cpp/ymq/pipe/pipe.h"
 
+Pipe::Pipe(): reader(-1), writer(-1)
+{
+    std::pair<long long, long long> pair = create_pipe();
+    this->reader                         = PipeReader(pair.first);
+    this->writer                         = PipeWriter(pair.second);
+}
+
+Pipe::Pipe(Pipe&& other) noexcept: reader(-1), writer(-1)
+{
+    this->reader = std::move(other.reader);
+    this->writer = std::move(other.writer);
+}
+
+Pipe& Pipe::operator=(Pipe&& other) noexcept
+{
+    this->reader = std::move(other.reader);
+    this->writer = std::move(other.writer);
+    return *this;
+}
+
 PipeReader::PipeReader(long long fd): _fd(fd)
 {
 }
